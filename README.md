bafd1e17f3ac97616e5f50c4ff873965f097adba
Christina Thee Roperto <christina.thee@gmail.com>
2022-10-13 23:24:22 +1100

    refactoring, finalising, adding README and examples

---
+++ README.md
@@ -0,0 +1,10 @@
+# Robot Movement
+
+Input can be from a file or from standard input.
+
+If input is from a file, run `./robot.py` followed by filename. Eg: `./robot.py example3.txt` 
+Output for example1 will be: `0,1,NORTH`
+
+### Things to implement in the future: 
+- Implementing tests
+- Perhaps adding something visual on the output
--- robot.py
+++ robot.py
@@ -33,16 +33,23 @@ class Robot:
 
     def place(self, position: str) -> None:
         position = position.split(",")
+        if len(position) != 3:
+            print("Invalid placement. Syntax: PLACE x,y,facing")
+            return
 
-        # try:
-        x = int(position[0].strip())
+        try:
+            x = int(position[0].strip())
+            y = int(position[1].strip())
+        except ValueError as e:
+            print(f"Invalid coordinate, must be an integer: {e}")
+            return
+
         if x > Robot.MAX_X or x < Robot.MIN_X:
             print(
                 f"Invalid x coordinate: {x} (max={Robot.MAX_X} and min={Robot.MIN_X})"
             )
             return
 
-        y = int(position[1].strip())
         if y > Robot.MAX_Y or y < Robot.MIN_Y:
             print(
                 f"Invalid y coordinate: {y} (max={Robot.MAX_Y} and min={Robot.MIN_Y})"
@@ -59,61 +66,53 @@ class Robot:
         self.y = y
         self.facing = facing
         self.placed = True
-
-        # except ValueError as e:
-        #     print("error type :", type(e))
-        #     if len(command) == 1:`
-        # print("please put correct command. Eg: place 0,0, WEST")
-        #
-
-        # # elif (x > 4 or x < 0) or (y > 4 or y < 0):
-        # #     print("please put correct position")
-        # if facing.upper() not in (Robot.NORTH, Robot.EAST, Robot.SOUTH, Tob.otWest):
-        # if facing.upper() not in ("NORTH", "EAST", "SOUTH", "WEST"):
-        #     print("invalid facing")
         # print("Initial command is: place at " + str(x) + ", " + str(y) + " and facing " + facing)
 
     def left(self) -> None:
-        if self.placed:
-            if self.facing == Robot.NORTH:
-                self.facing = Robot.WEST
-            elif self.facing == Robot.SOUTH:
-                self.facing = Robot.EAST
-            elif self.facing == Robot.WEST:
-                self.facing = Robot.SOUTH
-            elif self.facing == Robot.EAST:
-                self.facing = Robot.NORTH
-            print(self.facing)
+        if not self.placed:
+            print("Place the robot in the position first")
+            return
+
+        if self.facing == Robot.NORTH:
+            self.facing = Robot.WEST
+        elif self.facing == Robot.SOUTH:
+            self.facing = Robot.EAST
+        elif self.facing == Robot.WEST:
+            self.facing = Robot.SOUTH
+        elif self.facing == Robot.EAST:
+            self.facing = Robot.NORTH
         else:
-            print("please place the robot in the position first")
+            raise ValueError("Invalid current facing")
 
     def right(self) -> None:
-        if self.placed:
-            if self.facing == Robot.NORTH:
-                self.facing = Robot.EAST
-            elif self.facing == Robot.SOUTH:
-                self.facing = Robot.WEST
-            elif self.facing == Robot.WEST:
-                self.facing = Robot.NORTH
-            elif self.facing == Robot.EAST:
-                self.facing = Robot.SOUTH
-            print(self.facing)
+        if not self.placed:
+            print("Place the robot in the position first")
+            return
+
+        if self.facing == Robot.NORTH:
+            self.facing = Robot.EAST
+        elif self.facing == Robot.SOUTH:
+            self.facing = Robot.WEST
+        elif self.facing == Robot.WEST:
+            self.facing = Robot.NORTH
+        elif self.facing == Robot.EAST:
+            self.facing = Robot.SOUTH
         else:
-            print("please place the robot in the position first")
+            raise ValueError("Invalid current facing")
 
     def move(self) -> None:
-        if self.placed:
-            if self.facing == Robot.NORTH and self.y < 4:
-                self.y += 1
-            elif self.facing == Robot.SOUTH and self.y > 0:
-                self.y -= 1
-            elif self.facing == Robot.WEST and self.x > 0:
-                self.x -= 1
-            elif self.facing == Robot.EAST and self.x < 4:
-                self.x += 1
-            print(f"{self.x},{self.y},{self.facing}")
-        else:
-            print("illegal move. Please place the robot in the position first")
+        if not self.placed:
+            print("Place the robot in the position first")
+            return
+
+        if self.facing == Robot.NORTH and self.y < 4:
+            self.y += 1
+        elif self.facing == Robot.SOUTH and self.y > 0:
+            self.y -= 1
+        elif self.facing == Robot.WEST and self.x > 0:
+            self.x -= 1
+        elif self.facing == Robot.EAST and self.x < 4:
+            self.x += 1
 
     def report(self) -> None:
         if self.placed:
@@ -133,18 +132,22 @@ class Robot:
                 self.process_command(command)
 
     def process_command(self, command: List[str]) -> None:
-        if command[0] == "PLACE":
+        command_key = command[0]
+        if command_key == "":
+            return
+
+        if command_key == "PLACE":
             self.place(command[1])
-        elif command[0] == "LEFT":
+        elif command_key == "LEFT":
             self.left()
-        elif command[0] == "RIGHT":
+        elif command_key == "RIGHT":
             self.right()
-        elif command[0] == "MOVE":
+        elif command_key == "MOVE":
             self.move()
-        elif command[0] == "REPORT":
+        elif command_key == "REPORT":
             self.report()
         else:
-            print("invalid command. Please enter only place, move, left, right, report")
+            print(f"invalid command: {command}")
 
 
 if __name__ == "__main__":
